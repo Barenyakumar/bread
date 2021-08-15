2dc63516b0722673dbc13a76d2454e46590c3d8d
BarenyaKumar <barenyakumar@gmail.com>
2021-08-15 10:50:38 +0530

    index back to src

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Homepage from '../src/pages/homepage'
+import Homepage from './pages/homepage'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
 
